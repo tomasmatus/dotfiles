3eff8c8c445f1ba0ec93064412d455fe6dd83b5d
tomasmatus <tomatus777@gmail.com>
2024-05-20 22:09:10 +0200

    nvim: Add astrovim config

---
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,31 @@
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      "bash",
+
+      "c",
+      "cpp",
+
+      "lua",
+      "python",
+
+      "typescript",
+      "javascript",
+      "tsx",
+      "css",
+
+      "yaml",
+      "json",
+      "markdown_inline",
+
+      "make",
+      "dockerfile",
+      "vim",
+    })
+  end,
+}
