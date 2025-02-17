f7ed91a6eecf43c829c1a1581ddbc02173c2461a
tomasmatus <tomatus777@gmail.com>
2025-02-17 21:56:40 +0100

    nvim: set whichkey delay to 1s, add dashboard

--- private_dot_config/nvim/lua/kickstart/plugins/which-key.lua
+++ private_dot_config/nvim/lua/kickstart/plugins/which-key.lua
@@ -19,7 +19,9 @@ return {
     opts = {
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.opt.timeoutlen
-      delay = 0,
+      delay = 1,
+      -- window customization
+      preset = 'modern',
       icons = {
         -- set icon mappings to true if you have a Nerd Font
         mappings = vim.g.have_nerd_font,
